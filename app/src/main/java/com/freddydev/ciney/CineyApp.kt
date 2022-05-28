ec0c67abde3a517287c4da20b16bbb76856975ca
Donfreddy <freddytamwo@gmail.com>
2022-05-28 22:59:36 +0100

    Add dependency injection with Hilt

---
+++ app/src/main/java/com/freddydev/ciney/CineyApp.kt
@@ -0,0 +1,7 @@
+package com.freddydev.ciney
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CineyApp : Application() {}
--- app/src/main/java/com/freddydev/ciney/ui/main/MainActivity.kt
+++ app/src/main/java/com/freddydev/ciney/ui/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.freddydev.ciney
+package com.freddydev.ciney.ui.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.freddydev.ciney.ui.theme.CineyTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
