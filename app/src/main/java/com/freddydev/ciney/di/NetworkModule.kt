98c7f3b24d8853a10a9f7ddacc00f451149f9f5a
Don Freddy <freddytamwo@gmail.com>
2022-05-30 11:57:21 +0100

    Add build config field in app/build.gradle

--- app/src/main/java/com/freddydev/ciney/di/NetworkModule.kt
+++ app/src/main/java/com/freddydev/ciney/di/NetworkModule.kt
@@ -3,6 +3,7 @@ package com.freddydev.ciney.di
 import com.freddydev.ciney.service.CineyService
 import com.freddydev.ciney.util.interceptor.ApiKeyInterceptor
 import com.google.gson.Gson
+import com.freddydev.ciney.BuildConfig
 import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
@@ -16,8 +17,6 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
-private const val BASE_URL = "https://api.themoviedb.org/3"
-
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
@@ -26,7 +25,7 @@ object NetworkModule {
   @Singleton
   @IntoSet
   fun provideApiKeyInterceptor(): Interceptor {
-    return ApiKeyInterceptor(apiKey = "b838b78e9e0e25b39e41a2d3b197a08f")
+    return ApiKeyInterceptor(apiKey = BuildConfig.API_KEY)
   }
 
   @Provides
@@ -52,7 +51,7 @@ object NetworkModule {
   fun provideRetrofit(okHttpClient: OkHttpClient, gson: Gson): Retrofit {
     return Retrofit.Builder()
       .client(okHttpClient)
-      .baseUrl(BASE_URL)
+      .baseUrl(BuildConfig.TMDB_BASE_URL)
       .addConverterFactory(GsonConverterFactory.create(gson))
       .build()
   }
